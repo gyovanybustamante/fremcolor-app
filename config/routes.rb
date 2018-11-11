2726bf4a96805cd2bdf5f6a9b563c601c0069b4b
gyovanybustamante <gyovanybustamante@icloud.com>
2018-11-10 18:18:33 -0600

    prueba error scaffold
    prueba para solucionar el error del scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ Rails.application.routes.draw do
   resources :servicios
   resources :promocions
   resources :cursos
-  root 'home#index'
+
+  root 'cursos#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
